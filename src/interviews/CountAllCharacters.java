12d6efb18b8f7ca082b5b21cdd6226991a631457
Walé <56082759+OlaAwo@users.noreply.github.com>
2022-03-16 08:35:44 +0000

    added comments

--- src/interviews/CountAllCharacters.java
+++ src/interviews/CountAllCharacters.java
@@ -2,12 +2,18 @@ package interviews;
 
 public class CountAllCharacters {
     public static void main(String[] args) {
+
         String exampleString = "Sample string";
 
+        // use the length() function to get the length of a string
+        // get string length with spaces
         int stringLength = exampleString.length();
         System.out.println("String length: " + stringLength);
 
+        // get the string length without spaces
         int stringLengthWithoutSpaces = exampleString.replace(" ", "").length();
+
+        // print string length without spaces
         System.out.println("String length without counting whitespaces: " + stringLengthWithoutSpaces);
     }
 }
