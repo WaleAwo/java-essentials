2f29177f0db7fdc972233644b88b0188896c67ad
Walé <56082759+OlaAwo@users.noreply.github.com>
2022-03-20 09:00:14 +0000

    added a loop to check for duplicates

--- src/tasks/LotteryTicket.java
+++ src/tasks/LotteryTicket.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotteryTicket {
@@ -9,6 +10,7 @@ public class LotteryTicket {
 
     public static void main(String[] args) {
         int[] ticket = generateNumbers(); // call the method & store numbers
+        Arrays.sort(ticket);
         printTicket(ticket);
     }
 
@@ -17,9 +19,35 @@ public class LotteryTicket {
         Random random = new Random(); // generate a random number
 
         for (int i = 0; i < LENGTH; i++) {
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1; // assign & generate a number between 1 and 69
+            int randomNumber;
+
+            /*
+            Search random numbers to ensure there are no duplicates. If it does,
+            regenerate numbers
+             */
+            do {
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            }while (search(ticket, randomNumber));
+            ticket[i] = randomNumber;
         }
         return ticket;
+    }
+
+    /**
+     * Does a sequential search on the array to find value
+     * @param array Array to search through
+     * @param numberToSearchFor Value to search for
+     * @return true if found, false it not
+     */
+    public static boolean search(int[] array, int numberToSearchFor){
+
+        // Enhanced loop. It iterates through 'array' and assigns the current element to 'value'
+        for(int value: array){
+            if(value == numberToSearchFor){
+                return true;
+            }
+        }
+        return false; // no matching values found
     }
 
     public static void printTicket(int[] ticket) {
